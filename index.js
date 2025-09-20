0a995b528c7923b800a462eaba22a916c78746d0
Intaniamp <taniaputri483@gmail.com>
2025-09-20 17:47:43 +0800

    membuat function dari tambah, kurang, kali, bagi

---
+++ index.js
@@ -0,0 +1,6 @@
+import { tambah, kurang, kali, bagi } from "./calculator.js";
+
+console.log("2 + 3 =", tambah(2, 3));
+console.log("10 - 4 =", kurang(10, 4));
+console.log("7 x 6 =", kali(7, 6));
+console.log("20 / 5 =", bagi(20, 5));
